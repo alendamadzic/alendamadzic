dd031de165f6f4974f5fd4bdab23e43b86152b2b
Alen Damadzic <166737547+alendamadzic@users.noreply.github.com>
2025-01-13 17:02:15 +0000

    Fixed dark mode for the companies carousel

--- src/components/companies.tsx
+++ src/components/companies.tsx
@@ -4,34 +4,37 @@ const companies = [
   {
     name: 'Adarga',
     logo: '/adarga.avif',
+    needsInversion: false,
   },
   {
     name: 'Titania',
     logo: '/titania.svg',
+    needsInversion: true,
   },
   {
     name: 'Manchester City',
     logo: '/manchester_city.svg',
+    needsInversion: false,
   },
   {
     name: 'Splunk',
     logo: '/splunk.svg',
+    needsInversion: true,
   },
   {
     name: 'Improbable',
     logo: '/improbable.svg',
-  },
-  {
-    name: 'Elastic',
-    logo: '/elastic.svg',
+    needsInversion: true,
   },
   {
     name: 'Sony',
     logo: '/sony.webp',
+    needsInversion: true,
   },
   {
     name: 'ServiceNow',
     logo: '/servicenow.svg',
+    needsInversion: true,
   },
 ];
 
@@ -41,7 +44,12 @@ export function CompaniesSlider() {
       <h2 className="text-lg font-semibold text-muted-foreground">{"Some companies I've worked for and with..."}</h2>
       <InfiniteSlider gap={24} reverse>
         {companies.map((company) => (
-          <img key={company.name} src={company.logo} alt={company.name} className="h-[80px] w-auto max-w-[120px]" />
+          <img
+            key={company.name}
+            src={company.logo}
+            alt={company.name}
+            className={`h-[80px] w-auto max-w-[120px] ${company.needsInversion ? 'dark:invert dark:brightness-0' : ''}`}
+          />
         ))}
       </InfiniteSlider>
     </div>
