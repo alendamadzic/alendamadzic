4fc597bf246fe50f02f43e4c097da22a128053e2
Alen Damadzic <166737547+alendamadzic@users.noreply.github.com>
2025-01-15 20:22:59 +0000

    Fixed 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,5 @@
 import Image from 'next/image';
 
 export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen p-96">
-      <Image src="/404.gif" alt="404" fill />
-    </div>
-  );
+  return <Image src="/404.gif" alt="404" height={500} width={500} />;
 }
