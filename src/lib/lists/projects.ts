e3165bf7245b9056d01016469453d520e9726c9b
Alen Damadzic <alendamadzic@me.com>
2025-08-27 09:17:27 +0100

    Remove product-manager project

--- src/lib/lists/projects.ts
+++ src/lib/lists/projects.ts
@@ -6,13 +6,6 @@ export const projects = [
     icon: '/projects/viewpoint.png',
   },
   {
-    title: 'product manager.',
-    description:
-      'Emulate some of the admin involved in product management by turning ideas into artefacts to handover to stakeholders.',
-    link: 'https://pm.alen.world',
-    icon: '/projects/productmanager.png',
-  },
-  {
     title: 'games.',
     description: 'A series of sports knowledge games. ',
     link: 'https://games.alen.world',
