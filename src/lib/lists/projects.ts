9f96846fa4f55b66cc410372b50efbb1a87b4e35
Alen Damadzic <166737547+alendamadzic@users.noreply.github.com>
2025-09-27 20:36:13 +0100

    Added squad number to the projects

--- src/lib/lists/projects.ts
+++ src/lib/lists/projects.ts
@@ -3,12 +3,15 @@ export const projects = [
     title: 'Bubble PPM Pro',
     description: 'Project and Portfolio Management Software.',
     link: 'https://bubblegroup.com/',
-    icon: '/projects/bubble.png',
   },
   {
     title: 'Adarga Panoptic',
     description: 'Geopolitical perspectives using Generative AI',
     link: 'https://apps.apple.com/gb/app/adarga-panoptic/id6480463180',
-    icon: '/projects/panoptic.png',
+  },
+  {
+    title: 'squad number.',
+    description: "Quickly find out a football player's squad number.",
+    link: 'https://squadnumber.alen.world/',
   },
 ];
