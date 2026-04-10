02a2b6c2947cfb329c512ad786fc96778cd1f482
Alen Damadzic <alendamadzic@me.com>
2026-04-10 14:31:24 +0100

    Comment out Cadence project entry
    Commented out the Cadence project entry in the projects list.

--- src/lib/lists/projects.ts
+++ src/lib/lists/projects.ts
@@ -1,10 +1,10 @@
 export const projects = [
-  {
+  /*{
     title: 'cadence.',
     description:
       'Opinonated, AI-first, portfolio and project management based around the phase/stage gate methodology.',
     link: 'https://cadence.alen.world/',
-  },
+  },*/
   {
     title: 'Adarga Panoptic',
     description: 'Geopolitical perspectives using Generative AI',
